9ce74c941244fc88148704f2c460143b5af28666
cchristosgalanis <galanischris2@yahoo.com>
2026-05-12 15:34:27 +0300

    _

--- Producer_kafka.py
+++ Producer_kafka.py
@@ -1,6 +1,7 @@
 from confluent_kafka import Producer
 import json
 import time 
+import argparse
 from libpcap_approach import libpcap_capture
 
 def callback(err,msg):
@@ -11,6 +12,17 @@ def callback(err,msg):
         pass
 
 def Producer_func():
+
+    parser = argparse.ArgumentParser(description='Kafka Producer for Network Flow Data')
+    parser.add_argument('-i','--iface',
+                        type=str,
+                        default='en0',
+                        help='Network interface to capture traffic from (default: en0)'
+                )
+
+    args = parser.parse_args()
+    interface = args.iface
+
     conf = {
         'bootstrap.servers':'localhost:9092'
     }
@@ -24,7 +36,7 @@ def Producer_func():
         while True:
             #call libpcap function from libpcap_approach file to sniff network
             #check for arguments in function libpcap_capture
-            flow_data = libpcap_capture(capture_duration=1,interface='en0',flow_states=active_flow_states)
+            flow_data = libpcap_capture(capture_duration=1, interface=interface, flow_states=active_flow_states)
 
             if not flow_data.empty:
                 # iterate over all rows (all IPs)
